2bf1ce1db7df6090252c05f4c448c571eab6de3e
Prasoon Dheetal <107242581+Pra10soon@users.noreply.github.com>
2024-11-25 09:51:48 +0545

    added all the file

---
+++ netcentric/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace netcentric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10;
+            int secondNumber = 20;
+            Console.WriteLine("Before swapping");
+            Console.WriteLine($"First Number = {firstNumber}\nSecond Number = {secondNumber}");
+            firstNumber = firstNumber + secondNumber;
+            secondNumber = firstNumber - secondNumber;
+            firstNumber = firstNumber - secondNumber;
+            Console.WriteLine("After swapping");
+            Console.WriteLine($"First Number = {firstNumber}\n Second Number = {secondNumber}");
+
+        }
+    }
+}
